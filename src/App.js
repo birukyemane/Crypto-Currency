18a9859c7547e7b9664cf84e5727996677f9ca34
yemanebiruk <bir_yem@yahoo.com>
2018-11-13 00:27:18 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const API = 'https://api.coinmarketcap.com/v1/ticker/?limit=';
+const DEFAULT_QUERY = '20';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hits: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(API + DEFAULT_QUERY)
+      .then(response => response.json())
+      .then(data => this.setState({ hits: data }));
+  }
+
   render() {
+    const { hits } = this.state;
+    const displayCoins = hits.map(hit => <div key={hit.id}>{hit.name}</div>);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+     <div>
+       {displayCoins}
+     </div>
     );
   }
 }
